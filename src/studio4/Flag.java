c5583db3fe98e74849d6b650f1c33637cc3e50c1
nikitha <nikitha@DESKTOP-LNSBO6C.dhcp.wustl.edu>
2024-02-21 15:11:45 -0600

    push

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,15 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		 StdDraw.setPenRadius(0.02);
+	        StdDraw.setPenColor(StdDraw.BLACK);
+	        StdDraw.rectangle(0.5, 0.5, 0.4, 0.3);
+	        StdDraw.setPenColor(StdDraw.GREEN);
+	        StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.3);
+	        
+	        StdDraw.setPenColor(StdDraw.YELLOW);
+	        double[] x = { 0.2, 0.8, 0.7, 0.3 };
+	        double[] y = { 0.4, 0.4, 0.6, 0.6 };
+	        StdDraw.filledPolygon(x, y);
 	}
 }
